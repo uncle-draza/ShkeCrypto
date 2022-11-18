ed903956bd1d1fa290819ca1f76c56936ab6b6cf
dragoljub <dilpdrazovic@gmail.com>
2022-11-19 00:27:57 +0100

    zavrsena firebase autentifikacija

--- app/src/main/java/com/example/pma_projekat/SignInActivity.kt
+++ app/src/main/java/com/example/pma_projekat/SignInActivity.kt
@@ -8,21 +8,63 @@ import android.view.View
 import android.view.Window
 import android.view.WindowManager
 import android.widget.TextView
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.core.view.WindowCompat
+import com.example.pma_projekat.databinding.ActivitySignInBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class SignInActivity : AppCompatActivity()
 {
+    private lateinit var binding:ActivitySignInBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
-        requestWindowFeature(Window.FEATURE_NO_TITLE)
-        setContentView(R.layout.activity_sign_in)
 
+        //Uklanjanje title bar-a
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+
+        //Firebase Sign In
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        firebaseAuth = FirebaseAuth.getInstance()
+        binding.button.setOnClickListener {
+            val email = binding.emailET.text.toString()
+            val pass = binding.pswdET.text.toString()
+
+            if(email.isNotEmpty() && pass.isNotEmpty()) {
+                    firebaseAuth.signInWithEmailAndPassword(email,pass).addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            val intent = Intent(this, MainActivity::class.java)
+                            startActivity(intent)
+                        } else {
+                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            }
+            else {
+                Toast.makeText(this,"Fields cannot be empty!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        //Don't have an account? Sing up
         val switchToSignUpBtn = findViewById<TextView>(R.id.textView3)
         switchToSignUpBtn.setOnClickListener{
             val i = Intent(this, SignUpActivity::class.java)
             startActivity(i)
         }
     }
+
+    /*
+    //Pamti korisnika, pa aplikacija odmah salje korisnika na main activity, a ne svali put na sign in
+    override fun onStart() {
+        super.onStart()
+        if(firebaseAuth.currentUser != null) {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+     */
 }
--- app/src/main/java/com/example/pma_projekat/SignUpActivity.kt
+++ app/src/main/java/com/example/pma_projekat/SignUpActivity.kt
@@ -5,16 +5,55 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
 import android.widget.TextView
+import android.widget.Toast
+import com.example.pma_projekat.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class SignUpActivity : AppCompatActivity()
 {
+    private lateinit var binding:ActivitySignUpBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
+
+        //Uklanjanje title bar-a
         requestWindowFeature(Window.FEATURE_NO_TITLE)
-        setContentView(R.layout.activity_sign_up)
-        val switchToSignUpBtn = findViewById<TextView>(R.id.textView3)
-        switchToSignUpBtn.setOnClickListener{
+
+        //Firebase Sign Up
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        firebaseAuth = FirebaseAuth.getInstance()
+        binding.button.setOnClickListener{
+            val email = binding.emailET.text.toString()
+            val pass = binding.pswdET.text.toString()
+            val confpass = binding.confpswdET.text.toString()
+
+            if(email.isNotEmpty() && pass.isNotEmpty() && confpass.isNotEmpty()) {
+                if(pass == confpass) {
+                    firebaseAuth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener{
+                        if(it.isSuccessful) {
+                            val intent = Intent(this, SignInActivity::class.java)
+                            startActivity(intent)
+                        }
+                        else {
+                            Toast.makeText(this,it.exception.toString(), Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+                else {
+                    Toast.makeText(this,"Passwords do not match!", Toast.LENGTH_SHORT).show()
+                }
+            }
+            else {
+                Toast.makeText(this,"Fields cannot be empty!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        //Already have an account?
+        val switchToSignInBtn = findViewById<TextView>(R.id.textView3)
+        switchToSignInBtn.setOnClickListener{
             val i = Intent(this, SignInActivity::class.java)
             startActivity(i)
         }
