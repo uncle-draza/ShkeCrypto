9fbe4c7087e7e1987a1797c00672d39bf5648601
dragoljub <dilpdrazovic@gmail.com>
2022-11-18 17:41:10 +0100

    dodat prelaz izmedju sign up i in activity-a

--- app/src/main/java/com/example/pma_projekat/SignInActivity.kt
+++ app/src/main/java/com/example/pma_projekat/SignInActivity.kt
@@ -1,11 +1,13 @@
 package com.example.pma_projekat
 
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.Window
 import android.view.WindowManager
+import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.core.view.WindowCompat
 
@@ -16,5 +18,11 @@ class SignInActivity : AppCompatActivity()
         super.onCreate(savedInstanceState)
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_sign_in)
+
+        val switchToSignUpBtn = findViewById<TextView>(R.id.textView3)
+        switchToSignUpBtn.setOnClickListener{
+            val i = Intent(this, SignUpActivity::class.java)
+            startActivity(i)
+        }
     }
 }
--- app/src/main/java/com/example/pma_projekat/SignUpActivity.kt
+++ app/src/main/java/com/example/pma_projekat/SignUpActivity.kt
@@ -1,8 +1,10 @@
 package com.example.pma_projekat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
+import android.widget.TextView
 
 class SignUpActivity : AppCompatActivity()
 {
@@ -11,5 +13,10 @@ class SignUpActivity : AppCompatActivity()
         super.onCreate(savedInstanceState)
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_sign_up)
+        val switchToSignUpBtn = findViewById<TextView>(R.id.textView3)
+        switchToSignUpBtn.setOnClickListener{
+            val i = Intent(this, SignInActivity::class.java)
+            startActivity(i)
+        }
     }
 }
