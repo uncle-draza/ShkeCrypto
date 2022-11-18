4adc5d6bbe95dbe388da57f2a7a1f2c09230a0ad
dragoljub <dilpdrazovic@gmail.com>
2022-11-18 02:28:38 +0100

    manja podesavanja u login activity, signup activity gruba skica

--- app/src/main/java/com/example/pma_projekat/SignInActivity.kt
+++ app/src/main/java/com/example/pma_projekat/SignInActivity.kt
@@ -11,19 +11,10 @@ import androidx.core.view.WindowCompat
 
 class SignInActivity : AppCompatActivity()
 {
-
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
-
-        //window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
-
-
-        //window.attributes.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
         requestWindowFeature(Window.FEATURE_NO_TITLE)
-
-
-
         setContentView(R.layout.activity_sign_in)
     }
 }
--- app/src/main/java/com/example/pma_projekat/SignUpActivity.kt
+++ app/src/main/java/com/example/pma_projekat/SignUpActivity.kt
@@ -2,10 +2,14 @@ package com.example.pma_projekat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
 
-class SignUpActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class SignUpActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_sign_up)
     }
 }
