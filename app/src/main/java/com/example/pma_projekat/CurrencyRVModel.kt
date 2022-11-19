eaecc4c5b4cc5fd92cd2146edb63f770012847d0
dragoljub <dilpdrazovic@gmail.com>
2022-11-19 22:57:55 +0100

    napravljena kotlin model klasa i xml kartica

---
+++ app/src/main/java/com/example/pma_projekat/CurrencyRVModel.kt
@@ -0,0 +1,4 @@
+package com.example.pma_projekat
+
+class CurrencyRVModel (var name:String, var symbol:String, var price: Double){
+}
--- app/src/main/java/com/example/pma_projekat/MainActivity.kt
+++ app/src/main/java/com/example/pma_projekat/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.pma_projekat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.ProgressBar
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var searchEdit: EditText
+    private lateinit var currenciesRV: RecyclerView
+    private lateinit var loadingPB: ProgressBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        searchEdit = findViewById(R.id.idEditText)
+        currenciesRV = findViewById(R.id.idRVCurrencies)
+        loadingPB = findViewById(R.id.idPBLoading)
+
     }
 }
